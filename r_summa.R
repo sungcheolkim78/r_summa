5657255154279fb32b094f954ae3c619905a6c19
Sungcheol Kim <kimsung@us.ibm.com>
2020-01-09 18:14:12 -0500

    update r_summa

--- r_summa.R
+++ r_summa.R
@@ -6,6 +6,8 @@
 library(caret)
 library(purrr)
 library(doParallel)
+library(tibble)
+library(rlist)
 
 # summa S3 object
 
@@ -27,7 +29,7 @@ new_summa <- function(x = list(), method = "plus", fitControl = NULL) {
   structure(list(modellist = x, 
                  method = "summa", 
                  modelInfo = list(label="summa"),
-                 fitControl = fitControl,
+                 control = fitControl,
                  fitlist = list(), 
                  testlist = list(),
                  testproblist = list(), 
@@ -80,10 +82,10 @@ train.summa <- function(summa, formula, data, update=FALSE, n_cores=-1) {
     } else {
       #set.seed(1024)
       if (method %in% c('gbm', 'nnet'))       
-        caret::train(formula, data=data, method=method, trControl=summa$fitControl, 
+        caret::train(formula, data=data, method=method, trControl=summa$control, 
                      metric="ROC", tuneLength=4, preProc = c("center", "scale"), verbose=FALSE)
       else
-        caret::train(formula, data=data, method=method, trControl=summa$fitControl, 
+        caret::train(formula, data=data, method=method, trControl=summa$control, 
                      metric="ROC", tuneLength=4, preProc = c("center", "scale"))
     }
   }
@@ -97,38 +99,67 @@ train.summa <- function(summa, formula, data, update=FALSE, n_cores=-1) {
   summa
 }
 
-predict.summa <- function(summa, newdata = NULL, alpha=1.0, newmodellist = NULL, Y=NULL) {
-  if(!is.null(newdata)) 
-    summa$testdata <- newdata
-  stopifnot(!is.null(summa$testdata))
+predict.summa <- function(summa, newdata = NULL, Y=NULL, alpha=1.0, newmodellist = NULL, 
+                          method='summa+') {
+  msg <- paste0('... predict using ', method, ' , alpha: ', alpha)
+  message(msg)
+
+  # check test data set
+  if(!is.null(newdata)) summa$testdata <- newdata
+  stopifnot(length(summa$testdata) > 0)
   
-  summa$testlist <- map(summa$fitlist, predict, newdata=summa$testdata)
-  summa$testproblist <- map(summa$fitlist, predict, newdata=summa$testdata, type='prob')
-  
+  # check test class data set
   if(!is.null(Y)) summa$Y <- Y
+  stopifnot(length(summa$Y) > 0)
   
-  if(!is.null(summa$Y)) {
-    class1name = levels(summa$Y)[[1]]
-    class2name = levels(summa$Y)[[2]]
-    rho <- sum(summa$Y == class1name)/length(summa$Y)
-
-    summa$roclist <- map(summa$testproblist, rocrank, reference = summa$Y)
-    summa$lambdalist <- map(summa$roclist, lambda_fromROC, N=length(summa$Y), rho=rho)
+  # check models for training
+  if(is.null(newmodellist)) newmodellist <- names(summa$fitlist)
     
-    res <- cal_score(summa, alpha = alpha, newmodellist = newmodellist)
-    summa$testlist$summa <- as.factor(ifelse(res > 0, class2name, class1name))
-    summa$testproblist$summa <- data.frame(1/(1 + exp(res)), 1/(1+exp(-res)))
-    names(summa$testproblist$summa) <- c(class1name, class2name)
-    
-    summa$roclist <- c(summa$roclist, summa = rocrank(summa$testproblist$summa, reference = summa$Y))
-    summa$lambdalist$summa <- lambda_fromROC(summa$roclist$summa, N=length(summa$Y), rho=rho)
-    summa$confmatrix <- map(summa$testlist, confusionMatrix, reference = summa$Y)
-
-    # for debug
-    #print.summa(summa, newmodellist = newmodellist)
-    #print(summa$confmatrix$summa)
-    #print(summa$testproblist$summa)
+  if(length(summa$testlist) > 0) {
+    check <- newmodellist %in% names(summa$testlist)
+    newmodellist <- newmodellist[check]
+    if (!all(newmodellist %in% summa$modellist))
+      stop("Add model and train first")
+  } else {
+    message(paste0('... predict using initial ', length(summa$fitlist), ' classifiers'))
+    check <- newmodellist == ' '
+    summa$testlist <- map(summa$fitlist, predict, newdata=summa$testdata)
+    summa$testproblist <- map(summa$fitlist, predict, newdata=summa$testdata, type='prob')
   }
+  
+  # Y should be factor
+  class1name = levels(summa$Y)[[1]]
+  class2name = levels(summa$Y)[[2]]
+  summa$rho <- sum(summa$Y == class1name)/length(summa$Y)
+
+  # create probability matrix
+  if (any(!check)) {
+    message(paste0('... predict using additional ', sum(!check), ' classifiers'))
+    list.append(summa$testlist, map(summa$fitlist[!check], predict, newdata=summa$testdata))
+    list.append(summa$testproblist, map(summa$fitlist[!check], predict, newdata=summa$testdata, type='prob'))
+  } 
+  
+  # create roc, labmda, rstar list
+  summa$roclist <- map(summa$testproblist, rocrank, reference = summa$Y)
+  summa$lambdalist <- map(summa$roclist, lambda_fromROC, N=length(summa$Y), rho=summa$rho)
+    
+  # calculate new score using SUMMA algorithm
+  if (method == 'summa')
+    res <- cal_score_summa(summa, newmodellist = newmodellist)
+  else
+    res <- cal_score_summap(summa, alpha = alpha, newmodellist = newmodellist)
+  if (alpha > 1) method <- paste0('summa+', alpha)
+    
+  # add results
+  summa$testlist[[method]] <- as.factor(ifelse(res > 0, class2name, class1name))
+  summa$testproblist[[method]] <- data.frame(1/(1 + exp(res)), 1/(1+exp(-res)))
+  names(summa$testproblist[[method]]) <- c(class1name, class2name)
+    
+  summa$roclist[method] <- rocrank(summa$testproblist[[method]], reference = summa$Y)
+  summa$lambdalist[[method]] <- lambda_fromROC(summa$roclist[[method]], N=length(summa$Y), rho=summa$rho)
+  summa$confmatrix <- map(summa$testlist, confusionMatrix, reference = summa$Y)
+  summa$modelInfo$modelcheck <- names(summa$roclist) %in% c(newmodellist, method)
+  
   summa
 }
 
@@ -137,27 +168,30 @@ plot.summa <- function(summa) {
   dotplot(temp)
 }
 
-print.summa <- function(summa, newmodellist = NULL) {
-  print(summary.summa(summa, newmodellist = newmodellist))
+print.summa <- function(summa, full=TRUE) {
+  print(summary.summa(summa, full=full))
 }
 
-summary.summa <- function(summa, newmodellist = NULL) {
-  if (is.null(newmodellist)) 
-    newmodellist <- c(summa$modellist, 'summa')
-  else 
-    newmodellist <- c(newmodellist, 'summa')
+summary.summa <- function(summa, full=TRUE) {
+  # collect informations
+  ROC <- unlist(summa$roclist)
+  l1 <- map_dbl(summa$lambdalist, 1)
+  l2 <- map_dbl(summa$lambdalist, 2)
+  rs <- map_dbl(summa$lambdalist, 3)
   
-  ROC <- unlist(summa$roclist[newmodellist])
-  l1 <- map_dbl(summa$lambdalist[newmodellist], 1)
-  l2 <- map_dbl(summa$lambdalist[newmodellist], 2)
-  rs <- map_dbl(summa$lambdalist[newmodellist], 3)
-  #rbind(ROC, l1, l2, rs)
-  tibble(ROC=ROC, l1=l1, l2=l2, rs=rs, model=unlist(newmodellist))
+  # prepare data.frame
+  res <- tibble(ROC=ROC, l1=l1, l2=l2, rs=rs, model=names(summa$roclist), 
+                chk=summa$modelInfo$modelcheck)
+  res <- res[order(res$ROC), ]
+
+  # return results
+  if (full) return(res)
+  res[res$chk, 1:5]
 }
 
 # Utility functions
 
-cal_score <- function(summa, newmodellist = NULL, alpha = 1.0, view = FALSE) {
+cal_score_summap <- function(summa, newmodellist = NULL, alpha = 1.0, view = FALSE) {
   if (is.null(newmodellist)) newmodellist <- summa$modellist
   
   class1name = levels(summa$Y)[[1]]
@@ -168,6 +202,31 @@ cal_score <- function(summa, newmodellist = NULL, alpha = 1.0, view = FALSE) {
   for(m in newmodellist) {
     res[ , m] <- rank(summa$testproblist[[m]][[class1name]])
     res[ , m] <- summa$lambdalist[[m]][[2]]^alpha *(summa$lambdalist[[m]][[3]] - res[, m])
+  }
+  
+  if(view) {
+    temp <- as.data.frame(res)
+    temp[['summa+']] <- rowMeans(res)
+    names(temp)[-ncol(temp)] <- c(summa$modellist)
+    plot(temp)
+    print(cor(temp, method = "spearman"))
+  }
+  
+  rowMeans(res)
+}
+
+cal_score_summa <- function(summa, newmodellist = NULL, view = FALSE) {
+  if (is.null(newmodellist)) newmodellist <- summa$modellist
+  
+  class1name = levels(summa$Y)[[1]]
+  N = length(summa$Y)
+  
+  res <- matrix(0, nrow=length(summa$testlist[[1]]), ncol=length(newmodellist))
+  colnames(res) <- newmodellist
+  
+  for(m in newmodellist) {
+    res[ , m] <- rank(summa$testproblist[[m]][[class1name]])
+    res[ , m] <- 12*N*(summa$roclist[[m]] - 0.5)/(N*N - 1) *((N+1.)/2.- res[, m])
   }
   
   if(view) {
@@ -209,14 +268,28 @@ lambda_fromROC <- function(ROC, N=N, rho=rho) {
   return(c(l1 = l1, l2 = l2, rs = rs))
 }
 
+# calculate lambda1, lambda2 from ROC, rho (version 2)
+lambda_fromROC_appr <- function(ROC, N=N, rho=rho) {
+  l1_low <- log(1/rho - 1) - 12*N*(ROC-0.5)/(N*N-1)*((N+1+N*rho)*0.5 - N*rho*ROC)
+  l2_low <- 12*N*(ROC-0.5)/(N*N-1)
+  
+  temp <- sqrt(rho*(1-rho)*(1-2*(ROC-0.5)))
+  l1_high <- -2*rho/(sqrt(3)*temp)
+  l2_high <- 2/(sqrt(3)*N*temp)
+  
+  alpha <- 2*(ROC - 0.5)
+  l1 <- l1_high*alpha + l1_low*(1-alpha)
+  l2 <- l2_high*alpha + l2_low*(1-alpha)
+  return(c(l1low=l1_low, l2low=l2_low, l1high=l1_high, l2high=l2_high, l1=l1, l2=l2))
+}
+
 # make reports over multiple fittings
-generate_plot <- function(summalist, alpha=1, dataname='1') {
+generate_plot <- function(summalist, alpha=1, dataname='1', method='summa+', newmodellist=NULL) {
   res <- summalist %>%
-    map(predict, alpha=alpha) %>%
-    map(summary.summa) %>%
+    map(predict, alpha=alpha, method=method, newmodellist=newmodellist) %>%
+    map(summary.summa, full=FALSE) %>%
     reduce(rbind)
   
-  res[res$model == 'summa', 'model'] <- 'summa+'
   medians <- aggregate(ROC ~  model, res, median)
   
   g <- ggplot(res, aes(x=reorder(model, ROC, FUN = median), ROC, fill=model)) + 
@@ -225,8 +298,23 @@ generate_plot <- function(summalist, alpha=1, dataname='1') {
     ggtitle(paste0('Data Set ', dataname, ' - 10 iterations - alpha=', alpha)) +
     xlab('Models')
   
-  print(medians)
-  ggsave(paste0('DS', dataname, '_ROC_10iterations_a', alpha, '.pdf'))
-  g
+  ggsave(paste0('DS', dataname, '_ROC_10iterations_a', alpha, '_m', 
+    length(summalist[[1]]$modelInfo$modelcheck), '.pdf'))
+  print(g)
+  medians[order(medians$ROC), ]
 }
 
+buildSummaModel <- function(iter, ml=modellist, df=df, Y=Y, relation=Class~., dataname='1') {
+  set.seed(iter)
+  inTraining <- createDataPartition(Y, p=.75, list=FALSE)
+  training <- df[ inTraining, ]
+  testing  <- df[-inTraining, ]
+  testingY <- df[-inTraining, ncol(df)]
+
+  temp <- summa(modellist) %>%
+    train(relation, training, update=FALSE) %>%
+    predict(newdata=testing, Y=testingY, method='summa')
+
+  saveRDS(temp, file=paste0("DS", dataname, "_partition", iter, "_summa.RData"))
+  temp
+}
